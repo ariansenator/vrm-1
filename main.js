14b1a16788b794a540f7e22bea260705e66ec23f
ariansenator <ariansenator@gmail.com>
2020-06-15 16:54:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $("#bg-slider").owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 100,
+        autoPlay: 5000,
+        paginationSpeed: 100,
+        singleItem: true,
+        mouseDrag: false,
+        transitionStyle: "fade"
+                // "singleItem:true" is a shortcut for:
+                // items : 1, 
+                // itemsDesktop : false,
+                // itemsDesktopSmall : false,
+                // itemsTablet: false,
+                // itemsMobile : false 
+    });
+    $("#prop-smlr-slide_0").owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 100,
+        pagination: true,
+        paginationSpeed: 100,
+        items: 3
+
+    });
+    $("#testimonial-slider").owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 100,
+        pagination: true,
+        paginationSpeed: 100,
+        items: 3
+    });
+})
